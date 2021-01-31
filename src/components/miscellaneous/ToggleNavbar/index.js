e38a0af62e024c77fbccef456cb73b8a63e6e78e
AndresIOI <andres_ioi@hotmail.com>
2021-01-31 16:28:23 -0600

    add navbar responsive

--- src/components/miscellaneous/ToggleNavbar/index.js
+++ src/components/miscellaneous/ToggleNavbar/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'gatsby'
 import './index.scss'
 
 const ToggleNavbar = () => {
@@ -10,8 +11,24 @@ const ToggleNavbar = () => {
     }
 
     return (
-        <div className={`toggle-button ${ open ? `toggle-button--open` : `toggle-button--close`}`} onClick={showMenu}> 
-            <div></div>
+        <div className="toggle-navbar">
+            <div className={`toggle-button ${ open ? `toggle-button--open` : `toggle-button--close`}`} onClick={showMenu}> 
+                <div></div>
+            </div>
+            <ul className={`navbar-responsive ${ open ? `navbar-responsive--open` : `navbar-responsive--close`}`}>
+                <li className="navbar-responsive__link">
+                    <Link to='/about'>About</Link>
+                </li>
+                <li className="navbar-responsive__link">
+                    <Link to='/about'>Movies</Link>
+                </li>
+                <li className="navbar-responsive__link">
+                    <Link to='/about'>Characters</Link>
+                </li>
+                <li className="navbar-responsive__link">
+                    <Link to='/about'>Shop</Link>
+                </li>
+            </ul>
         </div>
     );
 }
